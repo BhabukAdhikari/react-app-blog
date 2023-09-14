0d06cd70c45792504caab34b412183b15d5994f4
BhabukAdhikari <bhabukadhikari1177@gmail.com>
2023-09-14 16:25:44 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,285 @@
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="blog-page">
+      <div className="container">
+        <div className="row">
+          {/* main-blog-side  */}
+          <div className="col-12 col-md-12 col-lg-8">
+            <div className="row">
+              <div className="col-md-6">
+                {/* second-card-starts */}
+                <div className="card">
+                  <img src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*WJ6t0O7V4J_G2TqZeadrVw.jpeg" className="card-image" alt="Image" />
+                  <div className="card-body">
+                    {/* text-part-of-card  */}
+                    <h2 className="post-title">
+                      <a>Why should you use React Js for frontend Apps?
+                      </a>
+                    </h2>
+                    <div className="post-para">
+                      <p>React is a JavaScript-based UI development library. React uses Virtual DOM,
+                        thereby
+                        creating web applications faster.</p>
+                    </div>
+                    {/* profile-and-avatar  */}
+                    <div className="profile d-md-flex flex-wrap">
+                      <div>
+                        <span className="text-dark">
+                          By:
+                        </span>
+                        <span className="fw-normal">
+                          Rajiv Chaulagain
+                        </span>
+                      </div>
+                      <div>
+                        <span className="text-dark small fw-light">
+                          created at : 4th August, 2023</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* second-card-ends  */}
+              </div>
+              <div className="col-md-6">
+                {/* second-card-starts */}
+                <div className="card">
+                  <img src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*WJ6t0O7V4J_G2TqZeadrVw.jpeg" className="card-image" alt="Image" />
+                  <div className="card-body">
+                    {/* text-part-of-card  */}
+                    <h2 className="post-title">
+                      <a>Why should you use React Js for frontend Apps?
+                      </a>
+                    </h2>
+                    <div className="post-para">
+                      <p>React is a JavaScript-based UI development library. React uses Virtual DOM,
+                        thereby
+                        creating web applications faster.</p>
+                    </div>
+                    {/* profile-and-avatar  */}
+                    <div className="profile d-md-flex flex-wrap">
+                      <div>
+                        <span className="text-dark">
+                          By:
+                        </span>
+                        <span className="fw-normal">
+                          Rajiv Chaulagain
+                        </span>
+                      </div>
+                      <div>
+                        <span className="text-dark small fw-light">
+                          created at : 4th August, 2023</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* second-card-ends  */}
+              </div>
+              <div className="col-md-6">
+                {/* second-card-starts */}
+                <div className="card">
+                  <img src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*WJ6t0O7V4J_G2TqZeadrVw.jpeg" className="card-image" alt="Image" />
+                  <div className="card-body">
+                    {/* text-part-of-card  */}
+                    <h2 className="post-title">
+                      <a>Why should you use React Js for frontend Apps?
+                      </a>
+                    </h2>
+                    <div className="post-para">
+                      <p>React is a JavaScript-based UI development library. React uses Virtual DOM,
+                        thereby
+                        creating web applications faster.</p>
+                    </div>
+                    {/* profile-and-avatar  */}
+                    <div className="profile d-md-flex flex-wrap">
+                      <div>
+                        <span className="text-dark">
+                          By:
+                        </span>
+                        <span className="fw-normal">
+                          Rajiv Chaulagain
+                        </span>
+                      </div>
+                      <div>
+                        <span className="text-dark small fw-light">
+                          created at : 4th August, 2023</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* second-card-ends  */}
+              </div>
+              <div className="col-md-6">
+                {/* second-card-starts */}
+                <div className="card">
+                  <img src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*WJ6t0O7V4J_G2TqZeadrVw.jpeg" className="card-image" alt="Image" />
+                  <div className="card-body">
+                    {/* text-part-of-card  */}
+                    <h2 className="post-title">
+                      <a>Why should you use React Js for frontend Apps?
+                      </a>
+                    </h2>
+                    <div className="post-para">
+                      <p>React is a JavaScript-based UI development library. React uses Virtual DOM,
+                        thereby
+                        creating web applications faster.</p>
+                    </div>
+                    {/* profile-and-avatar  */}
+                    <div className="profile d-md-flex flex-wrap">
+                      <div>
+                        <span className="text-dark">
+                          By:
+                        </span>
+                        <span className="fw-normal">
+                          Rajiv Chaulagain
+                        </span>
+                      </div>
+                      <div>
+                        <span className="text-dark small fw-light">
+                          created at : 4th August, 2023</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* second-card-ends  */}
+              </div>
+              <div className="col-md-6">
+                {/* second-card-starts */}
+                <div className="card">
+                  <img src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*WJ6t0O7V4J_G2TqZeadrVw.jpeg" className="card-image" alt="Image" />
+                  <div className="card-body">
+                    {/* text-part-of-card  */}
+                    <h2 className="post-title">
+                      <a>Why should you use React Js for frontend Apps?
+                      </a>
+                    </h2>
+                    <div className="post-para">
+                      <p>React is a JavaScript-based UI development library. React uses Virtual DOM,
+                        thereby
+                        creating web applications faster.</p>
+                    </div>
+                    {/* profile-and-avatar  */}
+                    <div className="profile d-md-flex flex-wrap">
+                      <div>
+                        <span className="text-dark">
+                          By:
+                        </span>
+                        <span className="fw-normal">
+                          Rajiv Chaulagain
+                        </span>
+                      </div>
+                      <div>
+                        <span className="text-dark small fw-light">
+                          created at : 4th August, 2023</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* second-card-ends  */}
+              </div>
+              <div className="col-md-6">
+                {/* second-card-starts */}
+                <div className="card">
+                  <img src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*WJ6t0O7V4J_G2TqZeadrVw.jpeg" className="card-image" alt="Image" />
+                  <div className="card-body">
+                    {/* text-part-of-card  */}
+                    <h2 className="post-title">
+                      <a>Why should you use React Js for frontend Apps?
+                      </a>
+                    </h2>
+                    <div className="post-para">
+                      <p>React is a JavaScript-based UI development library. React uses Virtual DOM,
+                        thereby
+                        creating web applications faster.</p>
+                    </div>
+                    {/* profile-and-avatar  */}
+                    <div className="profile d-md-flex flex-wrap">
+                      <div>
+                        <span className="text-dark">
+                          By:
+                        </span>
+                        <span className="fw-normal">
+                          Rajiv Chaulagain
+                        </span>
+                      </div>
+                      <div>
+                        <span className="text-dark small fw-light">
+                          created at : 4th August, 2023</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* second-card-ends  */}
+              </div>
+            </div>
+          </div>
+          {/* side-bar-right */}
+          <div className="col-12 col-md-12 col-lg-4">
+            <div className="side-bar-right">
+              {/* search-bar-starts  */}
+              <div className="search-part">
+                <form>
+                  <label className="search-label section-title ">Search</label>
+                  <div className="search-bar">
+                    <input className="search-input" placeholder="Search ..." />
+                    <button type="submit" className="search-btn">
+                      <i className="magnifying-glass fa-solid fa-magnifying-glass" />
+                    </button>
+                  </div>
+                </form>
+              </div>
+              {/* search-bar-ends  */}
+              {/* sidebar-widget-starts  */}
+              <div className="sidebar-widget"><label className="sidebar-widget-topic section-title">Popular
+                posts</label>
+                <ul className="thumbnail-items p-0">
+                  {/* post-1  */}
+                  <li className="post-list d-flex mb-3">
+                    <a>
+                      <img className="thumbnail" src="https://modtel.travelerwp.com/wp-content/uploads/2022/04/Rectangle-3-150x150.jpg" alt="Meet the Steve Jobs of the Travel Industry" decoding="async" loading="lazy" />
+                    </a>
+                    <div className="thumb-list-caption ps-3">
+                      <h3 className="thumb-list-title"><a>
+                        Meet the Steve Jobs of the Travel Industry</a></h3>
+                      <p className="thumb-list-date">May 6, 2022</p>
+                    </div>
+                  </li>
+                  {/* post-2  */}
+                  <li className="post-list d-flex mb-3">
+                    <a>
+                      <img className="thumbnail" src="https://modtel.travelerwp.com/wp-content/uploads/2022/04/Rooftop-View-1-150x150.jpg" alt="A Seaside Reset in Laguna Beach" decoding="async" loading="lazy" />
+                    </a>
+                    <div className="thumb-list-caption ps-3">
+                      <h3 className="thumb-list-title"><a>
+                        A Seaside Reset in Laguna Beach</a></h3>
+                      <p className="thumb-list-date">April 6, 2022</p>
+                    </div>
+                  </li>
+                  {/* post-3  */}
+                  <li className="post-list d-flex mb-3">
+                    <a>
+                      <img className="thumbnail" src="https://modtel.travelerwp.com/wp-content/uploads/2022/04/wili-150x150.jpg" alt="Tiptoe through the Tulips of Washington" decoding="async" loading="lazy" />
+                    </a>
+                    <div className="thumb-list-caption ps-3">
+                      <h3 className="thumb-list-title"><a>Tiptoe through the Tulips of Washington</a>
+                      </h3>
+                      <p className="thumb-list-date">April 6, 2022</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              {/* sidebar-widget-ends  */}
+            </div>
+          </div>
+          {/* side-bar-right-ends  */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
